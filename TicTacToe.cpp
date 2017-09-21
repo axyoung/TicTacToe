e03c6be9c4ae4925937d55b1778964d0014f60f9
cixay <alex.shan.young@gmail.com>
2017-09-21 12:20:16 -0700

    I am using my old java tictactoe to write this program

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,15 +2,31 @@
  * Alex Young
  * C++ Period 5
  * This program runs a 2 player game of Tictactoe
- * 9/19/2017
+ * 9/21/2017
  */
 
-#include <iostream>;
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+void printBoard();
 
 int main() {
-
-
+	char board[3][3];
+	printBoard();
 
 return 0;
+}
+
+//My old java TicTacToe code for printBoard, checkWin functions
+void printBoard() {
+	cout << " \t1\t2\t3" << endl;
+	for (int row = 0; row < 3; row++) {
+		char output = static_cast <char> ('a' + row);	
+		for (int column = 0; column < 3; column++) {
+			// what happens when on a turn goes here X or O...
+		}
+		cout << output << '\t' << endl;
+	}
+
 }
