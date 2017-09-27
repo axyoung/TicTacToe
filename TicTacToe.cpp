46957014ca00e78d1d3fb87a75ccffae6d8db85d
cixay <alex.shan.young@gmail.com>
2017-09-27 11:10:57 -0700

    ahhh pointers

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,7 +2,7 @@
  * Alex Young
  * C++ Period 5
  * This program runs a 2 player game of Tictactoe
- * 9/25/2017
+ * 9/27/2017
  */
 
 #include <iostream>
@@ -13,7 +13,7 @@ void printBoard();
 void resetBoard();
 int checkWin(int player);
 int checkTie();
-
+//lots of problems, need to get the whole pointer thing figured out
 int main() {
 	int blank = 0;
 	int x_move = 1;
@@ -25,7 +25,7 @@ int main() {
 	cout << "This is a game of TicTacToe." << endl << "The goal is to get 3 in a row" << endl
 		<< "Player X goes firstm then Player O" << endl;
 	int board[3][3];
-	int (*p)[3] = board; // get this pointer stuff figured out
+	// int (*p)[3] = board; // get this pointer stuff figured out
 	printBoard();
 	while (playing == 0) {
 		while (checkWin(x_move) == 0 && checkWin(o_move) == 0 && checkTie() == 0) {
@@ -33,26 +33,58 @@ int main() {
 		}
 	}
 
-return 0;
+	return 0;
 }
 
 //My old java TicTacToe code for printBoard, checkWin functions
 void printBoard() {
 	cout << " \t1\t2\t3" << endl;
 	for (int row = 0; row < 3; row++) {
-		char output = static_cast <char> ('a' + row);	
+		char output[4] = static_cast <char> ('a' + row) + '\t';	
 		for (int column = 0; column < 3; column++) {
 			// what happens when on a turn goes here X or O...
+			if (board[row][column] == blank) {
+				output += "\t";
+			}
+			else if (board[row][column] == x_move) {
+				output += "X\t";
+			}
+			else if (board[row][column] == o_move) {
+				output += "O\t";
+			}
+
 		}
-		cout << output << '\t' << endl;
+		cout << output << endl;
 	}
 
 }
-void resetBoard() {
 
+void resetBoard() {
+// copy printboard and change so  if it is a xomove make it blank in board and printboard
 }
 int checkWin(int player) { // line below need fix board...
 	if (board[0][0] == player && board[0][1] == player && board[0][2] == player) {
+		return 1;
+	}
+	if (board[1][0] == player && board[1][1] == player && board[1][2] == player) {
+		return 1;
+	}
+	if (board[2][0] == player && board[2][1] == player && board[2][2] == player) {
+		return 1;
+	}
+	if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+		return 1;
+	}
+	if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+		return 1;
+	}
+	if (board[0][0] == player && board[1][0] == player && board[2][0] == player) {
+		return 1;
+	}
+	if (board[0][1] == player && board[1][1] == player && board[2][1] == player) {
+		return 1;
+	}
+	if (board[0][2] == player && board[1][2] == player && board[2][2] == player) {
 		return 1;
 	}
 	cout << player << endl;
@@ -61,7 +93,7 @@ int checkTie() {
 	for (int row = 0; row < 3; row++) {
 		for(int column = 0; column < 3; column++) {
 		// need to fix line below so board and blank can be read in...
-			if(board[row][column] == blank) {
+			if(board[row][column] == 0) {
 				return 1;
 			}
 		}
